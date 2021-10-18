22753798aa02aa31cc54fbd02612cf3caff018fe
jocelyntong9 <73870004+jocelyntong9@users.noreply.github.com>
2021-10-19 00:52:26 +0700

    Add files via upload

---
+++ data karyawan.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Karyawan</title>
+    <link href="https://fonts.googleapis.com/css2?family=Antic+Didone&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="background">
+<br>
+    <input type="text1" placeholder="ID">
+<br>
+    <input type="text1" placeholder="Nama">
+<br>
+    <input type="text1" placeholder="Departmen">
+    <input type="text2" placeholder="Cari">
+
+    <table class="container1">
+        <tr>
+            <td rowspan="2" width="30px">
+                No
+            </td>
+            <td rowspan="2"width="50px">
+                ID
+            </td>
+            <td rowspan="2"width="150px">
+                Nama
+            </td>
+            <td rowspan="2"width="150px">
+                Tanggal Lahir
+            </td>
+            <td rowspan="2"width="250px">
+                Alamat
+            </td>
+            <td rowspan="2"width="150px">
+                Nomor Telepon
+            </td>
+            <td rowspan="2"width="120px">
+                Posisi
+            </td>
+            <td rowspan="2"width="120px">
+                Departmen
+            </td>
+            <td rowspan="2"width="80px">
+                Foto
+            </td>
+            <td colspan="2" width="120px">
+                Aksi
+            </td> 
+        </tr>
+        <tr>
+            <td width="60px">
+                Ubah
+            </td>
+            <td width="60px">
+                Hapus
+            </td>
+        </tr>
+    </table>
+
+    <table class="container2">
+        <tr>
+            <td width="30px">
+                1
+            </td>
+            <td width="50px">
+                001
+            </td>
+            <td width="150px">
+               Eric
+            </td>
+            <td width="150px">
+                21-07-2002
+            </td>
+            <td width="250px">
+                Jl.Melati 07, Batam Center
+            </td>
+            <td width="150px">
+                085624840125
+            </td>
+            <td width="120px">
+                Akuntan
+            </td>
+            <td width="120px">
+                Keuangan
+            </td>
+            <td width="80px">
+                001.jpg</a>
+            </td>
+            <td width="60px">
+                <a href="*">
+                <div class="pic1"></div></a> 
+            </td>
+            <td width="60px">
+                <a href="*">
+                <div class="pic2"></div></a>
+            </td>       
+        </tr>
+    </table>
+
+    <table class="container2">
+        <tr>
+            <td width="30px">
+                2
+            </td>
+            <td width="50px">
+                002
+            </td>
+            <td width="150px">
+               Felix
+            </td>
+            <td width="150px">
+                18-07-2002
+            </td>
+            <td width="250px">
+                Jl.Mawar 117, Nagoya
+            </td>
+            <td width="150px">
+                097624674932
+            </td>
+            <td width="120px">
+                Manajer
+            </td>
+            <td width="120px">
+                Pemasaran
+            </td>
+            <td width="80px">
+                002.jpg
+            </td>
+            <td width="60px">
+                <a href="*">
+                <div class="pic1"></div></a> 
+            </td>
+            <td width="60px">
+                <a href="*">
+                <div class="pic2"></div></a>
+            </td>      
+        </tr>
+    </table>
+
+    <table class="container2">
+        <tr>
+            <td width="30px">
+                3
+            </td>
+            <td width="50px">
+                003
+            </td>
+            <td width="150px">
+               Jessy
+            </td>
+            <td width="150px">
+                04-06-2002
+            </td>
+            <td width="250px">
+                Jl.Harapan 23, Batu Ampar
+            </td>
+            <td width="150px">
+                081246882259
+            </td>
+            <td width="120px">
+                Fotografi
+            </td>
+            <td width="120px">
+                Produksi
+            </td>
+            <td width="80px">
+                003.jpg
+            </td>
+            <td width="60px">
+                <a href="*">
+                <div class="pic1"></div></a> 
+            </td>
+            <td width="60px">
+                <a href="*">
+                <div class="pic2"></div></a>
+            </td>     
+        </tr>
+    </table>
+
+    <table class="container2">
+        <tr>
+            <td width="30px">
+                4
+            </td>
+            <td width="50px">
+                004
+            </td>
+            <td width="150px">
+               Jocelyn
+            </td>
+            <td width="150px">
+                13-11-2002
+            </td>
+            <td width="250px">
+                Jl.Yos Sudarso 01, Sekupang
+            </td>
+            <td width="150px">
+                089724561888
+            </td>
+            <td width="120px">
+                Admin
+            </td>
+            <td width="120px">
+                Administrasi
+            </td>
+            <td width="80px">
+                004.jpg
+            </td>
+            <td width="60px">
+                <a href="*">
+                <div class="pic1"></div></a> 
+            </td>
+            <td width="60px">
+                <a href="*">
+                <div class="pic2"></div></a>
+            </td>      
+        </tr>
+    </table>
+
+    <table class="container2">
+        <tr>
+            <td width="30px">
+                5
+            </td>
+            <td width="50px">
+                005
+            </td>
+            <td width="150px">
+               Viona
+            </td>
+            <td width="150px">
+                11-08-2002
+            </td>
+            <td width="250px">
+                Jl.Frenky 05, Muka Kuning
+            </td>
+            <td width="150px">
+                088523495837
+            </td>
+            <td width="120px">
+                Staf
+            </td>
+            <td width="120px">
+                Personalia
+            </td>
+            <td width="80px">
+                005.jpg
+            </td>
+            <td width="60px">
+                <a href="*">
+                <div class="pic1"></div></a> 
+            </td>
+            <td width="60px">
+                <a href="*">
+                <div class="pic2"></div></a>
+            </td>      
+        </tr>
+    </table>
+
+    <a href="pengaturan.php"><button type="button" class="btn4">Kembali</button></a>
+    <a href="data karyawan.php"><button type="button" class="btn4">Simpan</button></a>
+   <button onclick="window.print();" type="button" class="btn4">Cetak</button></a>
+</body>
+</html>
---
+++ pengaturan.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengaturan</title>
+    <link href="https://fonts.googleapis.com/css2?family=Antic+Didone&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="a-sidebar">
+        <div class="a-logo">
+        </div>
+            <button type="submit" class="a-menu"><a href="#"><i class="profil" data-icon="bx:bxs-user"></i>&ensp;Profil</a></button>
+            <button type="submit" class="a-menu"><a href="#"><i class="rekapitulasi"></i>&ensp;Rekapitulasi</a></button>
+            <button type="submit" class="a-currentmenu"><a href="pengaturan.php"><i class="pengaturan" ></i>&ensp;Pengaturan</a></button>
+            <button type="submit" class="a-menu"><a href="keluar.php"><i class="exit" data-icon="icomoon-free:exit"></i>&ensp;Keluar</a></button>
+    </div>
+    
+    <div class="background">
+
+        <a href="data karyawan.php" class="btn1">     
+        <div class="icon1"></div>
+        <div class="text">Data<br>Karyawan</div></a>
+<br>
+<br>
+<br>
+        <a href="*" class="btn2">     
+        <div class="icon2"></div>
+        <div class="text">Jadwal<br>Karyawan</div></a>
+
+        <a href="*" class="btn3">     
+        <div class="icon3"></div>
+        <div class="text">Tambah Profil<br>Karyawan</div></a>
+    </div>
+</body>
+</html> 
